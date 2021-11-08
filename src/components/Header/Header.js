872f9a35e017a516822ee7abbafeecf29a76fe71
Cansolele <androiddevelop@yandex.ru>
2021-11-08 17:09:30 +0300

    header layout + adaptive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,18 @@
 import style from "./Header.module.css";
 const Header = () => {
-  return <header className={style.header}></header>;
+  return (
+    <header className={style.header}>
+      <div className={style.header_icon_container}>
+        <div className={style.header_icon}>ICON</div>
+      </div>
+      <div className={style.next_task}>
+        CC TaskBoard :: Следующая задача -{" "}
+        <a href="/#" className={style.link_to_task}>
+          Задача 1 (Чат-бот Джефф)
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;

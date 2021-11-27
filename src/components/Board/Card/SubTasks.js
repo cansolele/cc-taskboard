392a0e8883601a6cf3b2bf2a8c1d10117e7c88b7
cansolele <androiddevelop@yandex.ru>
2021-11-27 17:25:56 +0300

    subtask tags, buttons

--- src/components/Board/Card/SubTasks.js
+++ src/components/Board/Card/SubTasks.js
@@ -7,26 +7,40 @@ const SubTasks = ({ subTask }) => {
       <label>
         <input type="checkbox" defaultChecked={subTask.completed} />
         <h4 className={style.name_of_subtask}>{subTask.subTaskName}</h4>
-        <div className={style.subtasks_tags_container}>
-          {subTask.subTaskTags?.map((tag) => (
-            <div
-              style={{ backgroundColor: tag.tagColor }}
-              className={style.subtask_tag}
-            >
-              {tag.tagName}
-            </div>
-          ))}
+        <div className={style.subtask_buttons}>
+          {" "}
+          {!subTask.subTaskTags && (
+            <button className={style.add_tag_btn}>
+              <IconContext.Provider
+                value={{ className: style.add_tag_btn_icon }}
+              >
+                <BsPlusLg />
+              </IconContext.Provider>
+            </button>
+          )}
+          <button className={style.triple_colon_button}>
+            <span className={style.triple_colon_icon}>⁝</span>
+          </button>
         </div>
       </label>
 
-      <button className={style.add_tag_btn}>
-        <IconContext.Provider value={{ className: style.add_tag_btn_icon }}>
-          <BsPlusLg />
-        </IconContext.Provider>
-      </button>
-      <button className={style.triple_colon_button}>
-        <span className={style.triple_colon_icon}>⁝</span>
-      </button>
+      <div className={style.subtasks_tags_container}>
+        {subTask.subTaskTags?.map((tag) => (
+          <div
+            style={{ backgroundColor: tag.tagColor }}
+            className={style.subtask_tag}
+          >
+            {tag.tagName}
+          </div>
+        ))}
+        {subTask.subTaskTags && (
+          <button className={style.add_tag_btn}>
+            <IconContext.Provider value={{ className: style.add_tag_btn_icon }}>
+              <BsPlusLg />
+            </IconContext.Provider>
+          </button>
+        )}
+      </div>
     </li>
   );
 };
